494b7276723e16fb5eb85a38405f09d44a82a6ff
Вероника Скирдова <verka0222@gmail.com>
2023-09-27 04:59:30 +0300

    + lifecycle

--- app/src/main/java/com/example/a_lab1/LogRegActivity.java
+++ app/src/main/java/com/example/a_lab1/LogRegActivity.java
@@ -3,25 +3,33 @@ package com.example.a_lab1;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 
 public class LogRegActivity extends Activity {
+    private String TAG = "Жизненный цикл";
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_reg_act);
-
-        Button loginButton = findViewById(R.id.loginButton);
-        EditText passInput = findViewById(R.id.passInput);
-
     }
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//
+//        // Get the Camera instance as the activity achieves full user focus
+//        Toast.makeText(this, "ты дурак?", Toast.LENGTH_SHORT).show(); //TODO может тут что-то сделать?
+//    }
 
     public void sendLogin(View v) { // loginButton onClick
 
@@ -40,5 +48,67 @@ public class LogRegActivity extends Activity {
         Intent intent = new Intent(this, ListActivity.class);
         intent.putExtra("user", user);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Toast.makeText(getApplicationContext(), "onRestart()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onRestart()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onDestroy()");
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginSW:
+                textView.setText("Приложение уже было запущено!");
+                break;
+            case R.id.regSW:
+                finish();
+                break;
+
+            default:
+                break;
+        }
     }
 }
